f8b364004d979d13f5e0be25ca225bad63ebd566
jaehyeongg <whwo1124@naver.com>
2024-05-13 15:27:56 +0900

    yaml file 정리

--- AWS/main_240513/variable.tf
+++ AWS/main_240513/variable.tf
@@ -349,7 +349,7 @@ variable "ingress_controller_yaml" {
 variable "service_account_name" {
   description = "Name Values for EKS Service Account"
   type = list(string)
-  default = ["system:serviceaccount:dev:s3-sa"]
+  default = ["system:serviceaccount:dev:storage-sa"]
 }
 
 variable "oidc_role_name" {
---
+++ Yaml/AWS_apply.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Apply를 원하는 YAML 파일들의 경로 목록
+yaml_files=(
+    "code/namespace.yaml"
+    "code/nginx_cm.yaml"
+    "code/service_account.yaml"
+    "code/aws-db-dns.yaml"
+    "code/aws-secret.yaml"
+    "code/was-dep.yaml"
+    "code/was-svc.yaml"
+    "code/web-dep.yaml"
+    "code/web-svc.yaml"
+    "code/ingress.yaml"
+)
+# 각 YAML 파일을 순서대로 적용
+for file in "${yaml_files[@]}"; do
+    kubectl apply -f "$file"
+done
