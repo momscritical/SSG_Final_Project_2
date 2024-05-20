6aa4bab5f9a476b5034a6d0a2c68265750e3932d
Taeyang <xodid3446@gmail.com>
2024-05-20 14:46:46 +0900

    240520 Change Azure

--- Azure/Basic/db_and_storage.tf
+++ Azure/Basic/db_and_storage.tf
@@ -1,223 +1,88 @@
-# ############################## Blob Storage ##############################
-# # Storage Account
-# resource "azurerm_storage_account" "sa" {
-#   name                     = "ssgpangstoragebox"
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = azurerm_resource_group.rg.location
-#   account_tier             = "Standard"
-#   account_replication_type = "GRS"
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-
-#   depends_on = [ azurerm_resource_group.rg ]
-# }
-
-# # EndPoint for Blob Storage
-# resource "azurerm_private_endpoint" "storage_endpoint" {
-#   name                = "azure-storage-endpoint"
-#   location            = var.az_loc
-#   resource_group_name = azurerm_resource_group.rg.name
-#   subnet_id           = azurerm_subnet.ep_subnet.id
-
-#   private_service_connection {
-#     name                           = "azure-storage-connection"
-#     private_connection_resource_id = azurerm_storage_account.sa.id
-#     subresource_names              = ["blob"]
-#     is_manual_connection           = false
-#   }
-
-#   ip_configuration {
-#     name               = var.az_ep.storage_static_name
-#     private_ip_address = var.az_ep.storage_static_ip
-#     subresource_name   = "blob"
-#     # 위쪽 블록의 subresource_names에 맞춰서 써야합니다.
-#   }
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-
-#   depends_on = [azurerm_storage_account.sa]
-# }
-
-# # Create Storage Container
-# resource "azurerm_storage_container" "sc" {
-#   name                  = "ssgpangstoragecontainer"
-#   storage_account_name  = azurerm_storage_account.sa.name
-#   # 공개? 접근 범위
-#   container_access_type = "container"
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-
-# ############################## Azure SQL Database ##############################
-# # Create Database
-# resource "azurerm_mysql_flexible_server" "mysql_server" {
-#   name                   = "ssgpang-db-server"
-#   resource_group_name    = azurerm_resource_group.rg.name
-#   location               = azurerm_resource_group.rg.location
-#   administrator_login    = var.db_username
-#   administrator_password = var.db_password
-#   sku_name               = "GP_Standard_D2ds_v4"
-#   zone                   = "2"
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-
-#   depends_on = [ azurerm_resource_group.rg ]
-# }
-
-# # EndPoint for Database
-# resource "azurerm_private_endpoint" "db_endpoint" {
-#   name                = "azure-db-endpoint"
-#   location            = var.az_loc
-#   resource_group_name = azurerm_resource_group.rg.name
-#   subnet_id           = azurerm_subnet.ep_subnet.id
-
-#   private_service_connection {
-#     name                           = "azure-db-connection"
-#     private_connection_resource_id = azurerm_mysql_flexible_server.mysql_server.id
-#     subresource_names              = ["mysqlServer"]
-#     is_manual_connection           = false
-#   }
-
-#   ip_configuration {
-#     name               = var.az_ep.db_static_name
-#     private_ip_address = var.az_ep.db_static_ip
-#     subresource_name   = "mysqlServer"
-#     # 위쪽 블록의 subresource_names에 맞춰서 써야합니다.
-#   }
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-
-#   depends_on = [azurerm_mysql_flexible_server.mysql_server]
-# }
-
-# # Set Server Parameter
-# resource "azurerm_mysql_flexible_server_configuration" "setting01" {
-#   name                = "require_secure_transport"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   server_name         = azurerm_mysql_flexible_server.mysql_server.name
-#   value               = "OFF"
-
-#   depends_on = [
-#     azurerm_resource_group.rg,
-#     azurerm_mysql_flexible_server.mysql_server
-#   ]
-# }
-
-# resource "azurerm_mysql_flexible_server_configuration" "setting02" {
-#   name                = "character_set_server"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   server_name         = azurerm_mysql_flexible_server.mysql_server.name
-#   value               = "UTF8MB4"
-
-#   depends_on = [
-#     azurerm_resource_group.rg,
-#     azurerm_mysql_flexible_server.mysql_server
-#   ]
-# }
-
-# resource "azurerm_mysql_flexible_server_configuration" "setting03" {
-#   name                = "collation_server"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   server_name         = azurerm_mysql_flexible_server.mysql_server.name
-#   value               = "UTF8MB4_GENERAL_CI"
-
-#   depends_on = [
-#     azurerm_resource_group.rg,
-#     azurerm_mysql_flexible_server.mysql_server
-#   ]
-# }
-
-# # Create Database
-# resource "azurerm_mysql_flexible_database" "ssgpang" {
-#   name                = "ssgpang"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   server_name         = azurerm_mysql_flexible_server.mysql_server.name
-#   charset             = "utf8mb4"
-#   collation           = "utf8mb4_general_ci"
-
-#   depends_on = [
-#     azurerm_resource_group.rg, 
-#     azurerm_mysql_flexible_server.mysql_server
-#   ]
-# }
-
-
-
-
-# storage account
+############################## Blob Storage ##############################
+# Storage Account
 resource "azurerm_storage_account" "sa" {
-    name                     = "ssgpangstoragebox"
-    resource_group_name      = azurerm_resource_group.rg.name
-    location                 = azurerm_resource_group.rg.location
-    account_tier             = "Standard"
-    account_replication_type = "GRS"
-    lifecycle {
-        create_before_destroy = true
-    }
-    depends_on = [ azurerm_resource_group.rg ]
+  name                     = "ssgpangstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [ azurerm_resource_group.rg ]
 }
+
+# EndPoint for Blob Storage
 resource "azurerm_private_endpoint" "storage_endpoint" {
-    name                = "azure-storage-endpoint"
-    location            = var.az_loc
-    resource_group_name = azurerm_resource_group.rg.name
-    subnet_id           = azurerm_subnet.ep_subnet.id
-    private_service_connection {
-        name                           = "azure-storage-connection"
-        private_connection_resource_id = azurerm_storage_account.sa.id
-        subresource_names              = [ "blob" ]
-        is_manual_connection           = false
-    }
-    ip_configuration {
-        name = var.az_ep.storage_static_name
-        private_ip_address = var.az_ep.storage_static_ip
-        subresource_name = "blob" 
-        # 위쪽 블럭의  subresource_names에 맞춰서 써야?
-    }
-    lifecycle {
-        create_before_destroy = true
-    }
-    depends_on = [ azurerm_storage_account.sa ]
+  name                = "ssgpang-storage-endpoint"
+  location            = var.az_loc
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.ep_subnet.id
+
+  private_service_connection {
+    name                           = "azure-storage-connection"
+    private_connection_resource_id = azurerm_storage_account.sa.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+
+  ip_configuration {
+    name               = var.az_ep.storage_static_name
+    private_ip_address = var.az_ep.storage_static_ip
+    subresource_name   = "blob"
+    # 위쪽 블록의 subresource_names에 맞춰서 써야합니다.
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [azurerm_storage_account.sa]
 }
+
+# Create Storage Container
 resource "azurerm_storage_container" "sc" {
-    name                  = "ssgpangstoragecontainer"
-    storage_account_name  = azurerm_storage_account.sa.name
-    # 공개? 접근 범위
-    container_access_type = "container"
-    lifecycle {
-        create_before_destroy = true
-    }
+  name                  = "ssgpangcontainer"
+  storage_account_name  = azurerm_storage_account.sa.name
+  # 공개? 접근 범위
+  container_access_type = "container"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+############################## Azure SQL Database ##############################
+# Create Database
 resource "azurerm_mysql_flexible_server" "mysql_server" {
-    name                   = "ssgpang-db-server"
-    resource_group_name    = azurerm_resource_group.rg.name
-    location               = azurerm_resource_group.rg.location
-    administrator_login    = var.db_username
-    administrator_password = var.db_password
-    sku_name               = "GP_Standard_D2ds_v4"
-    zone = "2"
-    lifecycle {
-        create_before_destroy = true
-    }
-    depends_on = [ azurerm_resource_group.rg ]
+  name                   = "ssgpang-db-server"
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  administrator_login    = var.db_username
+  administrator_password = var.db_password
+  sku_name               = "GP_Standard_D2ds_v4"
+  zone                   = "2"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [ azurerm_resource_group.rg ]
 }
 
+# Set Server Parameter
 resource "azurerm_mysql_flexible_server_configuration" "setting01" {
   name                = "require_secure_transport"
   resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_mysql_flexible_server.mysql_server.name
   value               = "OFF"
-  depends_on = [ azurerm_mysql_flexible_server.mysql_server ]
+
+  depends_on = [
+    azurerm_resource_group.rg,
+    azurerm_mysql_flexible_server.mysql_server
+  ]
 }
 
 resource "azurerm_mysql_flexible_server_configuration" "setting02" {
@@ -225,7 +90,11 @@ resource "azurerm_mysql_flexible_server_configuration" "setting02" {
   resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_mysql_flexible_server.mysql_server.name
   value               = "UTF8MB4"
-  depends_on = [ azurerm_mysql_flexible_server.mysql_server ]
+
+  depends_on = [
+    azurerm_resource_group.rg,
+    azurerm_mysql_flexible_server.mysql_server
+  ]
 }
 
 resource "azurerm_mysql_flexible_server_configuration" "setting03" {
@@ -233,38 +102,175 @@ resource "azurerm_mysql_flexible_server_configuration" "setting03" {
   resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_mysql_flexible_server.mysql_server.name
   value               = "UTF8MB4_GENERAL_CI"
-  depends_on = [ azurerm_mysql_flexible_server.mysql_server ]
+
+  depends_on = [
+    azurerm_resource_group.rg,
+    azurerm_mysql_flexible_server.mysql_server
+  ]
 }
 
+# EndPoint for Database
 resource "azurerm_private_endpoint" "db_endpoint" {
-    name                = "azure-db-endpoint"
-    location            = var.az_loc
-    resource_group_name = azurerm_resource_group.rg.name
-    subnet_id           = azurerm_subnet.ep_subnet.id
-    private_service_connection {
-        name                           = "azure-db-connection"
-        private_connection_resource_id = azurerm_mysql_flexible_server.mysql_server.id
-        subresource_names              = [ "mysqlServer" ]
-        is_manual_connection           = false
-    }
-    ip_configuration {
-        name = var.az_ep.db_static_name
-        private_ip_address = var.az_ep.db_static_ip
-        subresource_name = "mysqlServer" 
-        # 위쪽 블럭의  subresource_names에 맞춰서 써야?
-    }
-    lifecycle {
-        create_before_destroy = true
-    }
-    depends_on = [ azurerm_mysql_flexible_server.mysql_server ]
+  name                = "azure-db-endpoint"
+  location            = var.az_loc
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.ep_subnet.id
+
+  private_service_connection {
+    name                           = "azure-db-connection"
+    private_connection_resource_id = azurerm_mysql_flexible_server.mysql_server.id
+    subresource_names              = ["mysqlServer"]
+    is_manual_connection           = false
+  }
+
+  ip_configuration {
+    name               = var.az_ep.db_static_name
+    private_ip_address = var.az_ep.db_static_ip
+    subresource_name   = "mysqlServer"
+    # 위쪽 블록의 subresource_names에 맞춰서 써야합니다.
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [azurerm_mysql_flexible_server.mysql_server]
 }
 
+# Create Database
 resource "azurerm_mysql_flexible_database" "ssgpang" {
   name                = "ssgpang"
   resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_mysql_flexible_server.mysql_server.name
   charset             = "utf8mb4"
   collation           = "utf8mb4_general_ci"
+
+  depends_on = [
+    azurerm_resource_group.rg, 
+    azurerm_mysql_flexible_server.mysql_server
+  ]
 }
 
 
+
+
+
+
+
+
+
+
+
+
+# # storage account
+# resource "azurerm_storage_account" "sa" {
+#     name                     = "ssgpangstoragebox"
+#     resource_group_name      = azurerm_resource_group.rg.name
+#     location                 = azurerm_resource_group.rg.location
+#     account_tier             = "Standard"
+#     account_replication_type = "GRS"
+#     lifecycle {
+#         create_before_destroy = true
+#     }
+#     depends_on = [ azurerm_resource_group.rg ]
+# }
+# resource "azurerm_private_endpoint" "storage_endpoint" {
+#     name                = "azure-storage-endpoint"
+#     location            = var.az_loc
+#     resource_group_name = azurerm_resource_group.rg.name
+#     subnet_id           = azurerm_subnet.ep_subnet.id
+#     private_service_connection {
+#         name                           = "azure-storage-connection"
+#         private_connection_resource_id = azurerm_storage_account.sa.id
+#         subresource_names              = [ "blob" ]
+#         is_manual_connection           = false
+#     }
+#     ip_configuration {
+#         name = var.az_ep.storage_static_name
+#         private_ip_address = var.az_ep.storage_static_ip
+#         subresource_name = "blob" 
+#         # 위쪽 블럭의  subresource_names에 맞춰서 써야?
+#     }
+#     lifecycle {
+#         create_before_destroy = true
+#     }
+#     depends_on = [ azurerm_storage_account.sa ]
+# }
+# resource "azurerm_storage_container" "sc" {
+#     name                  = "ssgpangstoragecontainer"
+#     storage_account_name  = azurerm_storage_account.sa.name
+#     # 공개? 접근 범위
+#     container_access_type = "container"
+#     lifecycle {
+#         create_before_destroy = true
+#     }
+# }
+
+# resource "azurerm_mysql_flexible_server" "mysql_server" {
+#     name                   = "ssgpang-db-server"
+#     resource_group_name    = azurerm_resource_group.rg.name
+#     location               = azurerm_resource_group.rg.location
+#     administrator_login    = var.db_username
+#     administrator_password = var.db_password
+#     sku_name               = "GP_Standard_D2ds_v4"
+#     zone = "2"
+#     lifecycle {
+#         create_before_destroy = true
+#     }
+#     depends_on = [ azurerm_resource_group.rg ]
+# }
+
+# resource "azurerm_mysql_flexible_server_configuration" "setting01" {
+#   name                = "require_secure_transport"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   server_name         = azurerm_mysql_flexible_server.mysql_server.name
+#   value               = "OFF"
+#   depends_on = [ azurerm_mysql_flexible_server.mysql_server ]
+# }
+
+# resource "azurerm_mysql_flexible_server_configuration" "setting02" {
+#   name                = "character_set_server"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   server_name         = azurerm_mysql_flexible_server.mysql_server.name
+#   value               = "UTF8MB4"
+#   depends_on = [ azurerm_mysql_flexible_server.mysql_server ]
+# }
+
+# resource "azurerm_mysql_flexible_server_configuration" "setting03" {
+#   name                = "collation_server"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   server_name         = azurerm_mysql_flexible_server.mysql_server.name
+#   value               = "UTF8MB4_GENERAL_CI"
+#   depends_on = [ azurerm_mysql_flexible_server.mysql_server ]
+# }
+
+# resource "azurerm_private_endpoint" "db_endpoint" {
+#     name                = "azure-db-endpoint"
+#     location            = var.az_loc
+#     resource_group_name = azurerm_resource_group.rg.name
+#     subnet_id           = azurerm_subnet.ep_subnet.id
+#     private_service_connection {
+#         name                           = "azure-db-connection"
+#         private_connection_resource_id = azurerm_mysql_flexible_server.mysql_server.id
+#         subresource_names              = [ "mysqlServer" ]
+#         is_manual_connection           = false
+#     }
+#     ip_configuration {
+#         name = var.az_ep.db_static_name
+#         private_ip_address = var.az_ep.db_static_ip
+#         subresource_name = "mysqlServer" 
+#         # 위쪽 블럭의  subresource_names에 맞춰서 써야?
+#     }
+#     lifecycle {
+#         create_before_destroy = true
+#     }
+#     depends_on = [ azurerm_mysql_flexible_server.mysql_server ]
+# }
+
+# resource "azurerm_mysql_flexible_database" "ssgpang" {
+#   name                = "ssgpang"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   server_name         = azurerm_mysql_flexible_server.mysql_server.name
+#   charset             = "utf8mb4"
+#   collation           = "utf8mb4_general_ci"
+# }
