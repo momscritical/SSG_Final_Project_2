47d620d102a7677701a0b97d788e80e1f7d92b41
Taeyang <xodid3446@gmail.com>
2024-05-21 10:07:31 +0900

    240521 Test

--- Azure/AWS_Azure_Connection/aws_subnet.tf
+++ Azure/AWS_Azure_Connection/aws_subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "subnet1" {
-  vpc_id            = data.aws_vpc.selected.id
+  vpc_id            = data.aws_vpc.vpc.id
   cidr_block        = var.aws_subnet_ip_block
   availability_zone = "${var.aws_loc}a"
 
