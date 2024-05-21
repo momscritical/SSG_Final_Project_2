7223c9152153b7bde50bb90a38e891b05e97c321
Taeyang <xodid3446@gmail.com>
2024-05-21 12:23:25 +0900

    240521 Remove Creating Code about VPN RT

--- Azure/AWS_Azure_Connection/outputs.tf
+++ Azure/AWS_Azure_Connection/outputs.tf
@@ -5,8 +5,3 @@ output "aws_azure_connection" {
 output "aws_azure_ip_range" {
   value = "aws: ${var.aws_vpc_ip_block} <---> azure: ${var.az_vnet_ip}"
 }
-
-# output "primary_connection_string" {
-#     value = data.azurerm_storage_account.sa.primary_connection_string
-#     sensitive = true
-# }
