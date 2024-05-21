548c3e1cbf1f0a3de887c29e647b070a8919a065
Taeyang <xodid3446@gmail.com>
2024-05-21 16:34:30 +0900

    240521 Edit Python

--- AWS/main_240513/main.tf
+++ AWS/main_240513/main.tf
@@ -254,11 +254,11 @@ module "final_copy" {
   ]
 }
 
-module "final_monitoring" {
-  source = "./modules/monitoring"
+# module "final_monitoring" {
+#   source = "./modules/monitoring"
 
-  prometheus_yaml_loc = var.prometheus_yaml_loc
-}
+#   prometheus_yaml_loc = var.prometheus_yaml_loc
+# }
 
 
 
