877f4644bd97cb2c4f7cffb52d7f12b2cf521d13
Taeyang <xodid3446@gmail.com>
2024-05-23 15:44:31 +0900

    240523 ShellScript Test

--- Yaml/AWS_apply.sh
+++ Yaml/AWS_apply.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Apply를 원하는 YAML 파일들의 경로 목록
-yaml_files=(
+base=(
     "code-base/ingress-controller.yaml"
     "code-base/namespace.yaml"
-    "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/ha/install.yaml"
+)
+app=(
     "code-base/nginx_cm.yaml"
     "code-aws/service_account.yaml"
     "code-aws/aws-db-dns.yaml"
@@ -13,14 +14,35 @@ yaml_files=(
     "code-base/was-svc.yaml"
     "code-base/web-dep.yaml"
     "code-base/web-svc.yaml"
+)
+argo=(
+    "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/ha/install.yaml"
+)
+ingress=(
     "code-aws/aws-ingress.yaml"
 )
 
-# 각 YAML 파일을 순서대로 적용
-for file in "${yaml_files[@]}"; do
-    if [ "$file" == "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/ha/install.yaml" ]; then
-        kubectl apply -f "$file" -n argocd
-    else
-        kubectl apply -f "$file"
-    fi
-done
+# YAML 파일 적용 함수
+apply_yaml() {
+    for file in "${@}"; do
+        kubectl apply -f "$file" "$@"
+    done
+}
+
+# Ingress Controller & NameSpace YAML 파일 적용
+apply_yaml "${base[@]}"
+
+# Application YAML 파일 적용
+apply_yaml "${app[@]}"
+
+# TLS 인증서를 Kubernetes 시크릿으로 추가
+kubectl create secret tls argo-secret --cert /home/konan/ssl/argo.crt --key /home/konan/ssl/argo.key -n argocd
+
+# ArgoCD YAML 파일 적용
+apply_yaml "${argo[@]}" -n argocd
+
+# Ingress YAML 파일 적용
+apply_yaml "${ingress[@]}" -n argocd
+
+# ArgoCD 초기 비밀번호 출력
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
