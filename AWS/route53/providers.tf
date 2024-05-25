eda36aafbbfc678052d859344be5405c40bde356
Taeyang <xodid3446@gmail.com>
2024-05-25 09:35:05 +0900

    240525 Route53 Terraform Test

--- AWS/route53/providers.tf
+++ AWS/route53/providers.tf
@@ -16,12 +16,12 @@ provider "aws" {
   region = var.region
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     config_path = "~/.kube/config"
+#   }
+# }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
+# provider "kubernetes" {
+#   config_path = "~/.kube/config"
+# }
--- AWS/vpc2eks/providers.tf
+++ AWS/vpc2eks/providers.tf
@@ -20,12 +20,6 @@ provider "aws" {
   region = var.region
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
