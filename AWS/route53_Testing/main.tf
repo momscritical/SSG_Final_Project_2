94d271cc756418468d06299f55f7632a45f5793b
Taeyang <xodid3446@gmail.com>
2024-05-27 09:21:44 +0900

    240527 Edit Azure Managed ID Role

--- AWS/route53_Testing/main.tf
+++ AWS/route53_Testing/main.tf
@@ -1,10 +1,10 @@
-resource "aws_route53_record" "www" {
-  zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "www.${data.aws_route53_zone.selected.name}"
-  type    = "A"
-  ttl     = "300"
-  records = ["10.0.0.1"]
-}
+# resource "aws_route53_record" "www" {
+#   zone_id = data.aws_route53_zone.selected.zone_id
+#   name    = "www.${data.aws_route53_zone.selected.name}"
+#   type    = "A"
+#   ttl     = "300"
+#   records = ["10.0.0.1"]
+# }
 
 # resource "aws_elb" "main" {
 #   name               = "foobar-terraform-elb"
